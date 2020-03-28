d51f06a5c99a181f6d1d14ed03330155452f1686
hildyabreu114 <hildyabreu@pursuit.org>
2020-03-28 15:42:39 -0400

    create BrowserCollectionViewCell

---
+++ AzureOverall/BrowseCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BrowseCollectionViewCell.swift
+//  AzureOverall
+//
+//  Created by hildy abreu on 3/28/20.
+//  Copyright © 2020 hildy abreu. All rights reserved.
+//
+
+import UIKit
+
+class BrowseCollectionViewCell: UICollectionViewCell {
+    
+}
