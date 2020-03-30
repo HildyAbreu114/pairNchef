eab3de73761e4abf714d6f4e6925329f6aeed1da
hildyabreu114 <hildyabreu@pursuit.org>
2020-03-29 21:18:48 -0400

    add gitignore file

---
+++ AzureOverall/View Controllers/BrowseRecipeViewController.swift
@@ -0,0 +1,110 @@
+//
+//  BrowseRecipeViewController.swift
+//  AzureOverall
+//
+//  Created by hildy abreu on 3/27/20.
+//  Copyright © 2020 hildy abreu. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class BrowseRecipeViewController: UIViewController {
+    
+    //    MARK: UI OBJECTS
+    
+    lazy var searchBar: UISearchBar = {
+        let searchRecipe = UISearchBar()
+        searchRecipe.isTranslucent = true
+        searchRecipe.barTintColor = #colorLiteral(red: 1, green: 0.8978595138, blue: 0.891151607, alpha: 1)
+        searchRecipe.alpha = 0.7
+        searchRecipe.placeholder = "Search for your recipe here!"
+        
+        return searchRecipe
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let browseCollection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        browseCollection.register(BrowseCollectionViewCell.self, forCellWithReuseIdentifier: "browseCell")
+        browseCollection.backgroundColor = .clear
+        browseCollection.delegate = self
+        browseCollection.dataSource = self
+        return browseCollection
+    }()
+    
+     lazy var backgroundImageView: UIImageView = {
+            let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+            backgroundImage.layer.cornerRadius = 20
+            backgroundImage.image = #imageLiteral(resourceName: "4")
+            backgroundImage.contentMode = UIView.ContentMode.scaleToFill
+        backgroundImage.backgroundColor = .white
+            return backgroundImage
+        }()
+
+
+
+    //MARK: LIFECYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubView()
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        searchBarConstraint()
+        collectionViewConstraint()
+    }
+        
+    private func addSubView() {
+        view.addSubview(backgroundImageView)
+
+        view.addSubview(searchBar)
+        
+        view.addSubview(collectionView)
+}
+
+
+//MARK: CONSTRAINTS
+
+  func searchBarConstraint() {
+    searchBar.anchors(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop:80, paddingLeft: 15, paddingRight: 15, height: 50)
+    }
+
+
+func collectionViewConstraint(){
+    collectionView.anchors(top: searchBar.bottomAnchor, bottom: view.bottomAnchor, left:view.leftAnchor, right: view.rightAnchor, paddingBottom: 80, paddingLeft: 15, paddingRight: 15)
+    
+
+}
+}
+
+//MARK: EXTENSIONS
+
+extension BrowseRecipeViewController: UICollectionViewDelegate {
+    
+}
+
+extension BrowseRecipeViewController:UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "browseCell", for: indexPath) as? BrowseCollectionViewCell else {return UICollectionViewCell()}
+        return cell
+    }
+}
+
+extension BrowseRecipeViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+     
+    let virticalCellCGSize = CGSize(width: 400, height: 400)
+    return virticalCellCGSize
+  }
+   
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+     
+    return UIEdgeInsets(top: 4.0,left: 4.0,bottom: 4.0,right: 4.0)
+  }
+}
