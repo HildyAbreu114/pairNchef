e07ffdea767ce8b3aa9c7af0ea56c0bc664bbf54
hildyabreu114 <hildyabreu@pursuit.org>
2020-04-01 02:08:30 -0400

    set up cartTableViewCell

--- AzureOverall/Cells/CartTableViewCell.swift
+++ AzureOverall/Cells/CartTableViewCell.swift
@@ -10,15 +10,57 @@ import UIKit
 
 class CartTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK: UI OBJECTS
+    
+    lazy var recipeImage : UIImageView = {
+        let cartImage = UIImageView()
+        cartImage.contentMode = .scaleAspectFill
+
+        cartImage.clipsToBounds = true
+        cartImage.layer.cornerRadius = 15
+        cartImage.layer.borderColor = #colorLiteral(red: 1, green: 0.8937508464, blue: 0.8871408105, alpha: 1)
+        cartImage.layer.borderWidth = 2
+
+        return cartImage
+    }()
+    
+    lazy var totalAmountLabel: UILabel = {
+       let totalLabel = UILabel()
+        totalLabel.font = UIFont (name: "optima-bold", size: 10)
+        totalLabel.text = "serving"
+        totalLabel.textAlignment = .center
+        totalLabel.textColor = #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)
+        totalLabel.backgroundColor = #colorLiteral(red: 0.9213426709, green: 0.6355389357, blue: 0.6106185317, alpha: 1)
+        totalLabel.alpha = 0.5
+        totalLabel
+    }()
+    
+    lazy var recipeTitleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = UIFont (name: "optima-bold", size: 14)
+        titleLabel.text = "serving"
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)
+        titleLabel.backgroundColor = #colorLiteral(red: 0.9213426709, green: 0.6355389357, blue: 0.6106185317, alpha: 1)
+        titleLabel.alpha = 0.5
+        return titleLabel
+    }()
+ 
+    
+    
+    //MARK: LIFECYCLE
+    
+    override init(style: UITableViewCell.CellStyle,reuseIdentifier: String?) {
+        super.init(style:style, reuseIdentifier: reuseIdentifier)
+}
+required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    //MARK: CONSTRAINTS
+    
+    func recipeTitleLabelConstraint() {
+           image.addSubview(nLabel)
+           nLabel.anchors(top: image.topAnchor, left: image.leftAnchor, right: image.rightAnchor, height: 75)
+       }
 }
