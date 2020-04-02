1bdcc142c577777319d4d6b30d96bae3b9daf070
hildyabreu114 <hildyabreu@pursuit.org>
2020-04-02 01:03:18 -0400

    set UI for ShoppingCartVC

--- AzureOverall/Cells/CartTableViewCell.swift
+++ AzureOverall/Cells/CartTableViewCell.swift
@@ -15,12 +15,10 @@ class CartTableViewCell: UITableViewCell {
     lazy var recipeImage : UIImageView = {
         let cartImage = UIImageView()
         cartImage.contentMode = .scaleAspectFill
-
         cartImage.clipsToBounds = true
         cartImage.layer.cornerRadius = 15
         cartImage.layer.borderColor = #colorLiteral(red: 1, green: 0.8937508464, blue: 0.8871408105, alpha: 1)
         cartImage.layer.borderWidth = 2
-
         return cartImage
     }()
     
@@ -32,7 +30,7 @@ class CartTableViewCell: UITableViewCell {
         totalLabel.textColor = #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1)
         totalLabel.backgroundColor = #colorLiteral(red: 0.9213426709, green: 0.6355389357, blue: 0.6106185317, alpha: 1)
         totalLabel.alpha = 0.6
-        totalLabel
+        return totalLabel
     }()
     
     lazy var recipeTitleLabel: UILabel = {
@@ -62,6 +60,7 @@ required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
     }
     
+   
     //MARK: CONSTRAINTS
     
     func recipeImageConstraints() {
